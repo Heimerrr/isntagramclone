edd380af1c8c601dc118b53514a6870f57fc51f7
Heimer <50571469+Heimerrr@users.noreply.github.com>
2022-03-16 17:58:43 +0900

    Update settings.py

--- Jinstargram/Jinstargram/settings.py
+++ Jinstargram/Jinstargram/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%@8jri3)u@u5^@-&w^f@&h9$6@en(9=t7_f^&6j-mpy3k$cp45'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
